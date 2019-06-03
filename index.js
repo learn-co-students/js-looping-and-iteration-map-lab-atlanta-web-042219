618ae2de97517256dc00286761d20c1c807c2a1d
Paula Tulis <tulis.paula@gmail.com>
2019-06-03 08:28:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+  const newArray = []
+  array.map(function(element){
+    return newArray.push(element.toLowerCase());
+  });
+  return newArray
+}
+
+function nameToAttributes(array){
+  return array.map(function(element){
+  const element1 = element.split(' ')[0];
+  const element2 = element.split(' ')[1];
+  return {firstName: element1, lastName: element2};
+  });
+}
+
+
+function attributesToPhrase(array){
+ return array.map(function(element){
+return `${element.name} is from ${element.hometown}`;
+ });
+}
