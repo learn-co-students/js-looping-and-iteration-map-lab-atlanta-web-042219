ca733528742c7096f924079775079f113457398f
Han Xu <hanxu27@gmail.com>
2019-06-02 22:33:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+function lowerCaseDrivers(list) {
+  return list.map(function(driver) {
+    return driver.toLowerCase();
+  });
+}
+
+function nameToAttributes(list) {
+  return list.map(function (driver) {
+    const name = driver.split(' ');
+    return Object.assign({firstName: name[0]}, {lastName: name[1]});
+  });
+}
+
+function attributesToPhrase(list) {
+  return list.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`
+  });
+}
